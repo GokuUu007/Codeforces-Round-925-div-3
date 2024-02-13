d9d2d2751572a84733cdf37d21f7307833b8687c
Akshat madhavan <112942320+GokuUu007@users.noreply.github.com>
2024-02-14 00:33:17 +0530

    Create e.cpp

---
+++ e.cpp
@@ -0,0 +1,81 @@
+/* 
+
+    ॐ हौं जूं सः ॐ भूर्भुवः स्वः 
+    ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् उर्वारुकमिव बन्धनान्मृत्योर्मुक्षीय मामृतात् 
+    ॐ स्वः भुवः भूः ॐ सः जूं हौं ॐ
+
+   @Author  : Akshat Madhavan
+   @Country : INDIA 
+   
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(x) x.begin(), x.end()
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+
+
+
+// warning - integer overflow : )
+
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> zero(n);
+    int digs = 0;
+    for (int i = 0, x; i < n; ++i) {
+        cin >> x;
+        digs += to_string(x).size();
+        while (x % 10 == 0) x /= 10, zero[i] += 1;
+    }
+
+    sort(all(zero), greater<int>());
+
+    for (int i = 0; i < n; ++i) {
+        if (i & 1 ^ 1) {
+            digs -= zero[i];
+        }
+    }
+
+    cout << (digs >= m + 1 ? "Sasha\n" : "Anna\n");
+
+} 
+
+int32_t main() {
+    
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    #ifndef ONLINE_JUDGE   
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("error.txt", "w", stderr);  
+    #endif  
+
+    int tc = 1;
+    cin >> tc;
+    for (int cases = 1; cases <= tc; ++cases) {
+        solve();
+        // cout << (solve() ? "YES\n" : "NO\n");
+        // cout << "Case # " << cases;
+    } 
+
+    #ifndef ONLINE_JUDGE
+        double timeTaken = 1000.0 * clock() / CLOCKS_PER_SEC;
+        cout << "\n[Finished in " << timeTaken << "ms]";
+        cerr << "\n[Finished in " << timeTaken << "ms]";
+    #endif
+
+    return 0;
+}
+
+//JAI SHREE RAM !!!!
